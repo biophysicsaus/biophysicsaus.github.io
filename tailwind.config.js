1ac69d06f3266a53332f72fab45cb93c217aa074
Abhishek Patil <abhishek@zeroth.me>
2021-10-14 09:56:33 +1100

    new typograph and icons update
    Signed-off-by: Abhishek Patil <abhishek@zeroth.me>

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,24 +4,33 @@ module.exports = {
   theme: {
     extend: {
       minWidth: {
-        '0': '0',
-        '1/4': '25%',
-        '1/2': '50%',
-        '3/4': '75%',
-        'full': '100%',
-       },
-       maxWidth: {
-        '1/4': '25%',
-        '1/2': '50%',
-        '3/4': '75%',
-       },
-       backgroundImage: {
-        'banner': "url('/assets/images/banner.png')",
-       }
+        0: "0",
+        "1/4": "25%",
+        "1/2": "50%",
+        "3/4": "75%",
+        full: "100%",
+      },
+      maxWidth: {
+        "1/4": "25%",
+        "1/2": "50%",
+        "3/4": "75%",
+      },
+      backgroundImage: {
+        banner: "url('/assets/images/banner.png')",
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            a: {
+              textDecoration: "none",
+            },
+          },
+        },
+      },
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/typography")],
+};
