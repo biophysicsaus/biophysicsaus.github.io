bb4316f695314621ca0273672bfbd7ca270f4d56
Abhishek Patil <abhishek@zeroth.me>
2021-10-13 20:53:42 +1100

    calendar template updated
    Signed-off-by: Abhishek Patil <abhishek@zeroth.me>

--- src/lib/calendar_template.js
+++ src/lib/calendar_template.js
@@ -1,7 +1,7 @@
 module.exports = (zoom, talk) => {
   return {
     title: `${talk.name}: Biophysics Australia Seminar Series`,
-    description: `Hi There!\n  Talk by ${talk.name} at Biophysics Australia Seminar Series.\n Zoom details: \n ${zoom.url}\n zoom id: ${zoom.id} password: ${zoom.password}.\n\n --\nRegards,\nTeam biophysicsaus.com`,
+    description: `Hi There!\nTalk by ${talk.name} at Biophysics Australia Seminar Series.\n Zoom details: \n ${zoom.url}\n zoom id: ${zoom.id} password: ${zoom.password}.\n\n --\nRegards,\nTeam biophysicsaus.com`,
     start: `${talk.date} ${talk.time}`,
     duration: [1, "hour"],
   };
